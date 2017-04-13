879b00a2756a4e1774c9b1d65a06641f950a19ff
John Kim <johnkim@innovationskills.net>
2017-04-13 21:16:17 +0900

    Create ChristmasTree05.ino

---
+++ ChristmasTree05.ino
@@ -0,0 +1,39 @@
+void setup() {
+  for (int i=7; i<12; i++){
+    pinMode(i, OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  int brightness = analogRead(A0);
+  int numberOn = 5 - int(brightness / 200);
+
+  if(numberOn == 0){  // numberOn이 0인경우는 무조건 꺼짐 
+    for(int i=7; i<12; i++){
+      digitalWrite(i, LOW);
+    }
+  } else{
+    digitalWrite(numberOn + 6, HIGH);
+    for(int i=7+numberOn; i<12; i++) {
+      digitalWrite(i, LOW);
+    }
+    for(int i=7; i<6+numberOn; i++) {
+      digitalWrite(i, LOW);
+    }
+  }
+  
+  
+  /* else {  
+    for(int i=7; i<7+numberOn; i++){ 
+      digitalWrite(i, HIGH);
+    }
+    for(int i=7+numberOn; i<12; i++){
+      digitalWrite(i, LOW);
+    }
+  }
+  */
+
+  Serial.println(numberOn);
+  delay(100);
+}
