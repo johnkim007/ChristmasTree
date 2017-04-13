42ec1f6c6bd7427412f7a3859103c6e569945dbe
John Kim <johnkim@innovationskills.net>
2017-04-13 20:30:03 +0900

    Update christmastree02.ino

--- christmastree02.ino
+++ christmastree02.ino
@@ -1,1 +1,18 @@
 
+void setup() {
+  for(int i=7; i<12; i++) {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop() {
+  for(int i=7; i<12; i++){
+    digitalWrite(i, HIGH);
+    delay(500);
+  }
+
+  for(int i=7; i<12; i++){
+    digitalWrite(i, LOW);
+  }
+  delay(500);
+}
