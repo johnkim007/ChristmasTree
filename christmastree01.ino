42c3a57bfbe676dc430240bd38ff9829838a07b9
John Kim <johnkim@innovationskills.net>
2017-04-13 20:18:52 +0900

    Create christmastree01.ino

---
+++ christmastree01.ino
@@ -0,0 +1,26 @@
+void setup() {
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(8, HIGH);
+  delay(500);
+  digitalWrite(9, HIGH);
+  delay(500);
+  digitalWrite(10, HIGH);
+  delay(500);
+  digitalWrite(11, HIGH);
+  delay(500);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(7, LOW);
+  delay(500);
+}
